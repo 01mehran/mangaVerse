c243043dad758f644946b559cee973c9f157a0c1
mehran <mehranmohamadi1311@gmail.com>
2026-05-21 14:37:48 +0330

    docs(tailwindcss): set up tailwindcss for styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 function App() {
   return (
-    <h1>
-      <mark>Manga App</mark>
+    <h1 className="text-3xl font-bold text-center mt-10">
+      <mark className="px-3">MangaVerse</mark>
     </h1>
   );
 }
