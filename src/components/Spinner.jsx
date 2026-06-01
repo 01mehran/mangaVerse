348050c19c0c9c3eda1ea7409245f69773104e42
mehran <mehranmohamadi1311@gmail.com>
2026-06-02 00:58:52 +0330

    style(spinner): update spinner color in light mode

--- src/components/Spinner.jsx
+++ src/components/Spinner.jsx
@@ -2,8 +2,13 @@ import { Riple } from "react-loading-indicators";
 
 export default function Spinner() {
   return (
-    <div className="absolute top-1/2 left-1/2 -translate-x-1/2 transform">
-      <Riple color="#ad46ff" size="medium" text="" textColor="" />
+    <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+      <span className="hidden dark:block">
+        <Riple color="#ad46ff" size="medium" text="" textColor="" />
+      </span>
+      <span className="block dark:hidden">
+        <Riple color="#4539f6" size="medium" text="" textColor="" />
+      </span>
     </div>
   );
 }
