6cfca893f7795db0382a1f0f15ff51aa07ebe254
mehran <mehranmohamadi1311@gmail.com>
2026-06-04 02:54:00 +0330

    feat: add smooth transition to back to top button when scrollY is more than 800

--- src/components/BackToTopBtn.jsx
+++ src/components/BackToTopBtn.jsx
@@ -1,10 +1,24 @@
 // React Hooks;
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 // Icons;
 import { ChevronUp } from "lucide-react";
 
 export default function BackToTopBtn() {
+  const [show, setShow] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setShow(window.scrollY > 800);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   const backToTop = () => {
     window.scrollTo({
       top: 0,
@@ -16,7 +30,7 @@ export default function BackToTopBtn() {
   return (
     <button
       onClick={backToTop}
-      className={`sm-bottom-7 fixed right-4 bottom-4 z-50 flex size-10 cursor-pointer items-center justify-center rounded-full bg-indigo-600 text-white shadow-lg transition-all duration-300 hover:-translate-y-1 sm:right-7 sm:size-12 dark:bg-purple-600`}
+      className={`sm-bottom-7 fixed right-4 bottom-4 z-50 flex size-10 cursor-pointer items-center justify-center rounded-full bg-indigo-600 text-white shadow-lg transition-all duration-300 ease-in hover:-translate-y-1 sm:right-7 sm:size-12 dark:bg-purple-600 ${show ? "translate-x-0 opacity-100" : "pointer-events-none translate-x-20 opacity-50"}`}
     >
       <ChevronUp size={20} />
     </button>
